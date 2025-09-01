293c485ed62968c5b8a58e1214c0819e45553b08
kylepatrick <kyle@kylepatrick.com>
2025-08-31 23:26:45 -0500

    inital commit

---
+++ API/Program.cs
@@ -0,0 +1,37 @@
+using System.Net.WebSockets;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+ 
+
+var app = builder.Build();
+
+
+app.MapControllers();
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DBInitializer.SeedData(context);
+
+}
+catch (Exception ex)
+{   
+var logger=services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex,"Migration error");
+    
+}
+app.Run();
